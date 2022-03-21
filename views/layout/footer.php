e539dd579a75b557582ba8af2ebbcf826cbc18b9
JulitoxD <91586835+JulitoxD@users.noreply.github.com>
2022-03-21 09:42:02 +0100

    Antes del merge

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,11 +1,10 @@
-<div>
-</div>
-	
+    <!-- FOOTER -->
+    <footer id="footer">
+        <p>Desarrollado por Víctor Robles WEB &copy; <?= date('Y') ?></p>
+    </footer>
 
-			<!-- PIE DE PÁGINA -->
-			<footer id="footer">
-				<p>Desarrollado por sinNombrePHP &copy; <?= date('Y') ?></p>
-			</footer>
-		</div>
-	</body>
+    <!-- SCRIPTS -->
+    <!-- BOOTSTRAP JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
 </html>
--- views/layout/header.php
+++ views/layout/header.php
@@ -1,45 +1,43 @@
 <!DOCTYPE HTML>
 <html lang="es">
-	<head>
-		<meta charset="utf-8" />
-		<title>Tienda de Camisetas</title>
-        <link rel="stylesheet" href="<?=base_url?>/css/styles.css" />
-                
-	</head>
-	<body>
-		<div id="container">
-			<!-- CABECERA -->
-			<header id="header">
-				<div id="logo">
-					<img src="assets/img/camiseta.png" alt="Camiseta Logo" />
-					<a href="index.php">
-						Campus virtual
-					</a>
-				</div>
-			</header>
+<head>
+    <meta charset="utf-8" />
+    <title>your_Schedule</title>
+    <link type="text/css" rel="stylesheet" href="./css/styles.css"/>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
 
-			<!-- MENU -->
-			<nav id="menu">
-				<ul>
-					<li>
-						<a href="#">Inicio</a>
-					</li>
-					<li>
-						<a href="#">Categoria 1</a>
-					</li>
-					<li>
-						<a href="#">Categoria 2</a>
-					</li>
-					<li>
-						<a href="#">Categoria 3</a>
-					</li>
-					<li>
-						<a href="#">Categoria 4</a>
-					</li>
-					<li>
-						<a href="#">Categoria 5</a>
-					</li>
-				</ul>
-			</nav>
+<body>
+    <div id="container">
+        <!-- HERO -->
+        <header id="header">
+            <div id="logo">
+                <img src="assets/img/camiseta.png" alt="Camiseta Logo" />
+                <a href="index.php">Campus virtual</a>
+            </div>
+        </header>
 
-			<div id="content">
+        <!-- NAVBAR -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 5</a>
+                </li>
+            </ul>
+        </nav>
